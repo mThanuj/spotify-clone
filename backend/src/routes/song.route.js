686cae01a3378b50ae9fc4e9e2b68bba591b0952
mThanuj <mullagurithanuj0@gmail.com>
2024-12-11 18:20:29 +0530

    songs routes and controller

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,17 @@
 import express from "express";
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
 const songRouter = express.Router();
 
-songRouter.get("/", (req, res) => {
-  res.send("Song route with GET method");
-});
+songRouter.get("/", protectRoute, requireAdmin, getAllSongs);
+songRouter.get("/featured", getFeaturedSongs);
+songRouter.get("/made-for-you", getMadeForYouSongs);
+songRouter.get("/trending", getTrendingSongs);
 
 export default songRouter;
